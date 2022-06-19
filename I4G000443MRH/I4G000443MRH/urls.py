50e3a3ff0217b40f1c6b9ee3129cedd671da310b
thenaijadev <bob@>
2022-06-18 21:27:30 -0700

    first commit

--- I4G000443MRH/I4G000443MRH/urls.py
+++ I4G000443MRH/I4G000443MRH/urls.py
@@ -1,4 +1,4 @@
-"""I4G000634QKH URL Configuration
+"""I4G000443MRH URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- I4G000443MRH/I4G000443MRH/wsgi.py
+++ I4G000443MRH/I4G000443MRH/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for I4G000634QKH project.
+WSGI config for I4G000443MRH project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G000634QKH.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G000443MRH.settings')
 
 application = get_wsgi_application()
